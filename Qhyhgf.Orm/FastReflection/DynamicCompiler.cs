c5719e9d8e821a2f675bf0300535f9d7d02e38d9
caoxiancc@live.cn <caoxiancc@live.cn>
2016-10-24 14:40:04 +0800

    项目名称修改

---
+++ Qhyhgf.Orm/FastReflection/DynamicCompiler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CSharp;
+
+namespace Qhyhgf.Orm
+{
+    public static class DynamicCompiler
+    {
+        /// <summary>
+        /// 从文件编译
+        /// </summary>
+        /// <param name="files">要编译的代码文件集合</param>
+        /// <param name="referenceAssemblyNames">引用程序集名称集合</param>
+        /// <param name="outputAssembly">输出dll名称</param>
+        /// <returns>返回异常信息</returns>
+        public static string CompileFromFile(string[] files, string[] referenceAssemblyNames, string outputAssembly)
+        {
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            CompilerParameters compilerParameters = new CompilerParameters(referenceAssemblyNames, outputAssembly);
+            CompilerResults complierResult = codeProvider.CompileAssemblyFromFile(compilerParameters, files);
+            if (complierResult.Errors.HasErrors)
+            {
+                StringBuilder message = new StringBuilder();
+                foreach (CompilerError err in complierResult.Errors)
+                {
+                    message.AppendFormat("(FileName:{0},ErrLine:{1}): error {2}: {3}", err.FileName, err.Line, err.ErrorNumber, err.ErrorText);
+                }
+                return message.ToString();
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ Qhyhgf.Orm/Utils/ExceptionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace Qhyhgf.Orm.Utils
+{
+    internal static class ExceptionHelper
+    {
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "Method may not be used in every assembly it is imported into")]
+        internal static ArgumentException CreateArgumentNullOrEmptyException(string paramName)
+        {
+            return new ArgumentException("Argument_Cannot_Be_Null_Or_Empty", paramName);
+        }
+    }
+}
