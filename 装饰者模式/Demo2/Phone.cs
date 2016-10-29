48938b2693e758dd5685b2da88dc7de7b6d8618b
Administrator <Administrator@XZ-201606241029>
2016-10-29 09:21:58 +0800

    企业号，异常消息更新

---
+++ 装饰者模式/Demo2/Phone.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 装饰者模式
+{
+    /// <summary>
+    /// 手机抽象类，即装饰者模式中的抽象组件类
+    /// </summary>
+    public abstract class Phone
+    {
+        public abstract void Print();
+    }
+    /// <summary>
+    /// 苹果手机，即装饰着模式中的具体组件类
+    /// </summary>
+    public class ApplePhone : Phone
+    {
+        /// <summary>
+        /// 重写基类方法
+        /// </summary>
+        public override void Print()
+        {
+            Console.WriteLine("开始执行具体的对象——苹果手机");
+        }
+    }
+    /// <summary>
+    /// 装饰抽象类,要让装饰完全取代抽象组件，所以必须继承自Photo
+    /// </summary>
+    public abstract class Decorator : Phone
+    {
+        private Phone phone;
+
+        public Decorator(Phone p)
+        {
+            this.phone = p;
+        }
+
+        public override void Print()
+        {
+            if (phone != null)
+            {
+                phone.Print();
+            }
+        }
+    }
+    /// <summary>
+    /// 贴膜，即具体装饰者
+    /// </summary>
+    public class Sticker : Decorator
+    {
+        public Sticker(Phone p)
+            : base(p)
+        {
+        }
+
+        public override void Print()
+        {
+            base.Print();
+
+            // 添加新的行为
+            AddSticker();
+        }
+
+        /// <summary>
+        /// 新的行为方法
+        /// </summary>
+        public void AddSticker()
+        {
+            Console.WriteLine("现在苹果手机有贴膜了");
+        }
+    }
+    /// <summary>
+    /// 手机挂件
+    /// </summary>
+    public class Accessories : Decorator
+    {
+        public Accessories(Phone p)
+            : base(p)
+        {
+        }
+
+        public override void Print()
+        {
+            base.Print();
+
+            // 添加新的行为
+            AddAccessories();
+        }
+
+        /// <summary>
+        /// 新的行为方法
+        /// </summary>
+        public void AddAccessories()
+        {
+            Console.WriteLine("现在苹果手机有漂亮的挂件了");
+        }
+    }
+}
--- 装饰者模式/Program.cs
+++ 装饰者模式/Program.cs
@@ -10,6 +10,7 @@ namespace 装饰者模式
     {
         static void Main(string[] args)
         {
+            #region demo 1
             Beverage beverage = new Espresso();
             //不要调料
             Console.WriteLine(beverage.getDescription()+" $ "+beverage.cost());
@@ -18,6 +19,28 @@ namespace 装饰者模式
             beverage2 = new Mocha(beverage2);
             Console.WriteLine(beverage2.getDescription() + " $ " + beverage2.cost());
 
+            #endregion;
+
+            // 我买了个苹果手机
+            Phone phone = new ApplePhone();
+
+            // 现在想贴膜了
+            Decorator applePhoneWithSticker = new Sticker(phone);
+            // 扩展贴膜行为
+            applePhoneWithSticker.Print();
+            Console.WriteLine("----------------------\n");
+
+            // 现在我想有挂件了
+            Decorator applePhoneWithAccessories = new Accessories(phone);
+            // 扩展手机挂件行为
+            applePhoneWithAccessories.Print();
+            Console.WriteLine("----------------------\n");
+
+            // 现在我同时有贴膜和手机挂件了
+            Sticker sticker = new Sticker(phone);
+            Accessories applePhoneWithAccessoriesAndSticker = new Accessories(sticker);
+            applePhoneWithAccessoriesAndSticker.Print();
+            Console.ReadLine();
         }
     }
 }
