580ac62d6869a0803cf83feebdeaa051ab3c82c2
Administrator <Administrator@XZ-201606241029>
2016-11-14 15:17:51 +0800

    修改

---
+++ Qhyhgf.WeiXin.Qy.Api.Test/SendMsg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Qhyhgf.WeiXin.Qy.Api.Token;
+using Qhyhgf.WeiXin.Qy.Api.Request;
+using Qhyhgf.WeiXin.Qy.Api.Response;
+using Qhyhgf.WeiXin.Qy.SeedMessage;
+
+namespace Qhyhgf.WeiXin.Qy.Api.Test
+{
+    [TestClass]
+    public class SendMsg
+    {
+        [TestMethod]
+        public void MediaUpload()
+        {
+            TokenManager manger = new TokenManager();
+            TokenEntity entiy = manger.GetToken("0");
+            IWeiXinClient client = new DefaultWeiXinClient();
+            client.Token = entiy;
+            MediaUploadRequest mediaUploadRequest =new MediaUploadRequest();
+            mediaUploadRequest.Media = @"D:\证件照片\DSCF8856.JPG";
+            mediaUploadRequest.Type = Domain.MediaType.Image;
+            MediaUploadResponse MediaUploadResponse = client.Execute<MediaUploadResponse>(mediaUploadRequest);
+            Assert.IsNotNull(MediaUploadResponse.MediaId);
+        }
+    }
+}
