e578d47c4d531fd5dd6e9a3ef88b3e84eeba6994
Administrator <Administrator@XZ-201606241029>
2018-09-28 15:44:27 +0800

    增加上传图片接口

---
+++ WeiXin.Api/Request/Media/MediaUploadimgRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Qhyhgf.WeiXin.Qy.Api.Attribute;
+using Qhyhgf.WeiXin.Qy.Api.Response;
+using Qhyhgf.WeiXin.Qy.Api.Domain;
+using System.Runtime.Serialization;
+
+namespace Qhyhgf.WeiXin.Qy.Api.Request
+{
+
+    /// <summary>
+    /// 上传图片
+    /// 上传图片得到图片URL，该URL永久有效
+    /// 返回的图片URL，仅能用于图文消息（mpnews）正文中的图片展示；若用于非企业微信域名下的页面，图片将被屏蔽。
+    /// 每个企业每天最多可上传100张图片
+    /// </summary>
+    [HttpMethod(Method = HttpVerb.File, Url = "https://qyapi.weixin.qq.com/cgi-bin/media/upload", Name = "上传图片", IsToken = true, Serialize = SerializeVerb.Json)]
+    public class MediaUploadimgRequest : IWeiXinRequest<MediaUploadimgResponse>
+    {
+        public MediaUploadimgRequest() {
+            Type = MediaType.Image;
+        }
+        /// <summary>
+        /// 媒体文件类型，分别有图片（image）、语音（voice）、视频（video），普通文件(file)
+        /// </summary>
+        [DataMember(Name = "type", IsRequired = false)]
+        public MediaType Type { get; set; }
+        /// <summary>
+        /// 多媒体路径
+        /// </summary>
+        [DataMember(IsRequired = true, Name = "media")]
+        public string Media { get; set; }
+    }
+}
---
+++ WeiXin.Api/Response/Media/MediaUploadimgResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Qhyhgf.WeiXin.Qy.Api.Domain;
+
+namespace Qhyhgf.WeiXin.Qy.Api.Response
+{
+    /// <summary>
+    /// 上传图片
+    /// </summary>
+    public class MediaUploadimgResponse: WeiXinResponse
+    {
+        /// <summary>
+        /// 上传后得到的图片URL。永久有效
+        /// </summary>
+        [DataMember(Name = "url")]
+         public string Url { get; set; }
+    }
+}
